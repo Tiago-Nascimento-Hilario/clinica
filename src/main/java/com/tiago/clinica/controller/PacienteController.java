74cf3d75e8bc8f5f955e52135ec54454ba0130eb
Tiago Nascimento Hilario <tnhhilario@gmail.com>
2025-03-27 23:59:14 -0300

    feat: adcionando novas depencias: validation e springdoc-openapi

--- src/main/java/com/tiago/clinica/controller/PacienteController.java
+++ src/main/java/com/tiago/clinica/controller/PacienteController.java
@@ -3,6 +3,7 @@ package com.tiago.clinica.controller;
 import com.tiago.clinica.model.Paciente;
 import com.tiago.clinica.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,7 +20,18 @@ public class PacienteController {
     }
 
     @PostMapping
-    public Paciente cadastrarPaciente(@RequestBody Paciente paciente){
+    public Paciente cadastrarPaciente( @Validated @RequestBody Paciente paciente){
        return pacienteRepository.save(paciente);
+    }
+
+    @PutMapping("/{id}")
+    public  void atualizaPaciente(@PathVariable Long id, @RequestBody Paciente pacienteAtualizado){
+        pacienteAtualizado.setId(id);
+        pacienteRepository.save(pacienteAtualizado);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePaciente(@PathVariable Long id){
+        pacienteRepository.deleteById(id);
     }
 }
--- src/main/java/com/tiago/clinica/model/Paciente.java
+++ src/main/java/com/tiago/clinica/model/Paciente.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
@@ -13,7 +15,9 @@ public class Paciente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Nome é obrigatório")
     private String nome;
+    @Email(message = "Email inválido")
     private String email;
     private String telefone;
 }
